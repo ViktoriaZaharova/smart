91df786d82a10992dda16bac75c2f62c949b3690
Viktoria <zaharova181114@gmail.com>
2021-02-20 06:09:41 +0200

    add page partners

--- app/js/common.js
+++ app/js/common.js
@@ -2,6 +2,11 @@
 //
 // });
 
+$('.partner-slider').slick({
+    slidesToShow: 1,
+    fade: true
+});
+
 $('.menu li a').each(function () {
     var location = window.location.href;
     var link = this.href;
